47103274bd12166c50d4a818bd588e5d66a9145b
Piyush1532 <piyushadav503@gmail.com>
2024-08-26 15:30:39 +0530

    added add functionality and passed through props to another component

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import todo_icon from "../assets/todo_icon.png"
+import TodoItems from './TodoItems'
+import uniqid from 'uniqid';
+
 const Todo = () => {
+ const inputRef=useRef();
+ const [todoList,setTodoList]=useState([])
+
+const addTodo=()=>{
+const inputText=inputRef.current.value.trim()
+
+if (inputText==='') {
+  return null;
+}else{
+  const newTodo={
+    id:uniqid(),
+    text:inputText,
+    isComplete:false
+   }
+   setTodoList((prev)=>[...prev,newTodo])
+   inputRef.current.value=""
+}
+
+
+}
+
+
+
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
 
@@ -10,6 +37,20 @@ const Todo = () => {
 </div>
     
 
+    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+      <input  className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"type="text" placeholder='Add your task' ref={inputRef} />
+      <button className='border-none rounded-full bg-orange-500 w-32 h-14 text-white text-lg font-medium cursor-pointer' onClick={addTodo}>Add +</button>
+    </div>
+
+
+
+<div>
+  {todoList.map((item,index)=>{
+return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete}/>
+  })}
+
+ 
+</div>
     </div>
   )
 }
